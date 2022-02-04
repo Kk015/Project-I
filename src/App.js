f6f4206be306e9ee9d88657e0a94fbf9366104de
Kunal <kunalmathur0803@gmail.com>
2022-02-04 13:43:57 +0530

    3

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         alt="n-bg"
         className="n-bg"
         width="1600"
-        height="770"
+        height="970"
       />
       <div className="but-top-right">
         <Button className="glob-but">
@@ -29,12 +29,12 @@ function App() {
       </div>
       <div className="section-1">
         <div className="f-1">Unlimited movies, TV shows and more.</div>
-        <div className="f-2">Watch anywhere. Cancel anytime.</div>
+        <div className="f-2 f-2-1">Watch anywhere. Cancel anytime.</div>
         <div className="f-3">
           Ready to watch? Enter your email to create or restart your membership.
         </div>
       </div>
-      <div class="input-group  email-input input-group-lg mb-3">
+      <div class="input-group mb-3 input-group-lg  email-input ">
         <input
           type="text"
           class="form-control "
@@ -50,6 +50,30 @@ function App() {
           Get Started
         </button>
       </div>
+      <div className="borderline bl1"></div>
+      <div className="bg-black d1">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div className="f-1 s-2-1">Enjoy Your TV</div>
+              <div className="f-2 f-2-2 s-2-2">
+                Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+                Blu-ray players and more.{" "}
+              </div>
+              <div class="col">
+                <img
+                  src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+                  alt="n-bg"
+                  className="tv-ph"
+                  width="600"
+                  height="400"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="borderline bl2"></div>
     </div>
   );
 }
