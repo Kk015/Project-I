48b79a6802aec08bd5cb54b2d4fdcee942546c5a
Kunal <kunalmathur0803@gmail.com>
2022-02-25 23:19:23 +0530

    3

--- src/Homepage/Home.js
+++ src/Homepage/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Rectangle2 from "../Images/Rectangle2.png";
 import "./Home.css";
 
 function Home() {
@@ -13,6 +12,16 @@ function Home() {
         <span className="Rectangle2-txt-2">
           142,765 Computer Engineers follow this
         </span>
+
+        <div class="pill-nav post-nav">
+          <a class="active" href="#home">
+            All Posts(32)
+          </a>
+          <a href="#news">Articule</a>
+          <a href="#contact">Event</a>
+          <a href="#about">Education</a>
+          <a href="#about">Job</a>
+        </div>
       </div>
     </div>
   );
