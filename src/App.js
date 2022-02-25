cb110d92b3cd8a6a68bdb51bd2cfa9b7d8da8d5a
Kunal <kunalmathur0803@gmail.com>
2022-02-25 22:57:48 +0530

    2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import Navbaro from "./Navbar/Navbar";
 import Home from "./Homepage/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <main>
       <Navbaro />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </main>
   );
 }
 
--- src/Homepage/Home.js
+++ src/Homepage/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Rectangle2 from "../Images/Rectangle2.png";
+import "./Home.css";
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div>
+        <div className="Rectangle2"></div>
+        <span className="Rectangle2-txt">
+          Computer Engineering <br />
+        </span>
+        <span className="Rectangle2-txt-2">
+          142,765 Computer Engineers follow this
+        </span>
+      </div>
     </div>
   );
 }
