dac472388026c06ce0ab076df3a9b28984d5a409
Kunal <kunalmathur0803@gmail.com>
2022-02-27 17:06:53 +0530

    4

--- src/Homepage/Home-main.jsx
+++ src/Homepage/Home-main.jsx
@@ -1,48 +1,77 @@
 import React from "react";
 import emojipedia from "./Home-content";
 import "./Home-main.css";
+import { Dropdown } from "react-bootstrap";
+import Share from "../Images/3dot.png";
 
 function Entry(props) {
-    return (
-        <div className="props-border">
-            <span > {props.image}<br /></span>
-            <div className="prop-content-padding">
-                <span className="type-css">{props.type}<br /></span>
-                <span className="heading-css">  {props.heading}<br /></span>
-                <span className="description-css">   {props.description}<br /></span>
-                <span className="description-css">   {props.nameimg}</span>
-                <span className="loc-img">   {props.loc}</span>
-                <span className="stamp">   {props.stamp}</span>
-                <span className="name-css">   {props.name} </span>
+  return (
+    <div className="props-border">
+      <span>
+        {" "}
+        {props.image}
+        <br />
+      </span>
+      <div className="prop-content-padding">
+        <span className="type-css">
+          {props.type}
+          <br />
+        </span>
+        <span className="heading-css">
+          {" "}
+          {props.heading}
+          <br />
+        </span>
 
-            </div>
-            <div className="view-share">
-                <div className="views"></div>
-                <div className="share"></div>
-            </div>
-        </div>
-    );
+        <Dropdown className="no-color">
+          <Dropdown.Toggle className="share1">
+            <img src={Share} alt="Share" />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Edit</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Report </Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <span className="description-css">
+          {" "}
+          {props.description}
+          <br />
+        </span>
+        <span className="description-css"> {props.nameimg}</span>
+        <span className="loc-img"> {props.loc}</span>
+        <span className="stamp"> {props.stamp}</span>
+        <span className="name-css"> {props.name} </span>
+      </div>
+      <div className="view-share">
+        <div className="views"></div>
+        <div className="share"></div>
+      </div>
+    </div>
+  );
 }
 function Homemain() {
-    return (
-        <div>
-            <dl className="dictionary">
-                {emojipedia.map((emojiTerm) => (
-                    <Entry
-                        key={emojiTerm.id}
-                        image={emojiTerm.image}
-                        type={emojiTerm.type}
-                        name={emojiTerm.name}
-                        description={emojiTerm.description}
-                        heading={emojiTerm.heading}
-                        nameimg={emojiTerm.nameimg}
-                        loc={emojiTerm.loc}
-                        stamp={emojiTerm.stamp}
-                    />
-                ))}
-            </dl>
-        </div>
-    );
+  return (
+    <div>
+      <dl className="dictionary">
+        {emojipedia.map((emojiTerm) => (
+          <Entry
+            key={emojiTerm.id}
+            image={emojiTerm.image}
+            type={emojiTerm.type}
+            name={emojiTerm.name}
+            description={emojiTerm.description}
+            heading={emojiTerm.heading}
+            nameimg={emojiTerm.nameimg}
+            loc={emojiTerm.loc}
+            stamp={emojiTerm.stamp}
+          />
+        ))}
+      </dl>
+    </div>
+  );
 }
 
 export default Homemain;
